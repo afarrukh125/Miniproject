77eb128b4ec4c147b7e318e76c06b2642b2e0a90
Abdullah Farrukh <33581953+afarrukh125@users.noreply.github.com>
2024-09-26 01:39:10 +0100

    Move to separate package

---
+++ src/main/java/me/afarrukh/miniproject/states/UITextButton.java
@@ -0,0 +1,41 @@
+package me.afarrukh.miniproject.states;
+
+import me.afarrukh.miniproject.gfx.Visuals;
+import me.afarrukh.miniproject.ui.ClickListener;
+import me.afarrukh.miniproject.ui.UIObject;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+class UITextButton extends UIObject {
+    private final ClickListener clickListener;
+    private final String text;
+
+    public UITextButton(String text, float x, int y, int width, int height, ClickListener clickListener) {
+        super(x, y, width, height);
+        this.text = text;
+        this.clickListener = clickListener;
+    }
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(Color.GRAY);
+        g.fillRect((int) x, (int) y, width, height);
+        if (isHovering()) {
+            g.setColor(Color.CYAN);
+        } else {
+            g.setColor(Color.BLACK);
+        }
+        g.setFont(Visuals.font28);
+        g.drawString(text, (int) x + (width / 7), (int) y + (height / 2));
+    }
+
+    @Override
+    protected void onClick() {
+        clickListener.onClick();
+    }
+}
